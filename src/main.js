55d9ad034ef9bccf550bb29a6eb4631a096199ef
Bhardwaj-Sameer <saming.program@gmail.com>
2024-10-02 17:31:20 +0530

    added character's idle and motion animations

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ k.loadSprite("spritesheet", "./spritesheet.png"), {
     sliceX: 39,
     sliceY: 31,
     anims: {
-        "idle-down" :936,
-        
+    "idle-down": 936,
+    "walk-down": {from: 936, to: 939, loop: true, speed: 8 },
+    "idle-side": 975,
+    "walk-side": {from: 975, to: 978, loop: true, speed: 8}, //This will be flipped for the Left
+    "idle-up": 1014,
+    "walk-up": {from: 1014, to: 1017, loop: true, speed: 8},
     }
 }
