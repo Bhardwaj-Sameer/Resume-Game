a78e42fcf7dbf198692f457fd3be2dcbf4fd61fe
Bhardwaj-Sameer <saming.program@gmail.com>
2024-10-05 11:41:50 +0530

    Added interval ref in utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,18 @@
 export function displayDialogue(text, onDisplayEnd){
     const dialogueUI = document.getElementById("textbook-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dialogueUI.style.display = "block";
+
+    let index=0;
+    let currentText= "";
+    const intervalRef = setInterval(() => {
+        if(index<text.length){
+            currentText+= text[index];
+            dialogue.innerHTML = currentText;
+            index++;
+            return;
+        }
+        clearInterval(intervalRef);
+    },5);
 }
