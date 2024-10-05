9ce8bd4375fb12ecd708b21e0d11a64a03be2d18
Bhardwaj-Sameer <saming.program@gmail.com>
2024-10-05 12:21:26 +0530

    added close button to utils

--- src/utils.js
+++ src/utils.js
@@ -15,4 +15,17 @@ export function displayDialogue(text, onDisplayEnd){
         }
         clearInterval(intervalRef);
     },5);
+
+
+    const closeBtn = document.getElementById("close");
+
+    function onCloseBtnClick(){
+        onDisplayEnd();
+        dialogueUI.style.display = "none";
+        dialogue.innerHTML ="";
+        clearInterval(intervalRef);
+        closeBtn.removeEventListener("click", onCloseBtnClick);
+    }
+
+    closeBtn.removeEventListener("click", onCloseBtnClick);
 }
